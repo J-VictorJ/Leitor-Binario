8e66f6f335c630ebd392a8279e1f2c15f6049538
J-VictorJ <114741655+J-VictorJ@users.noreply.github.com>
2024-04-17 00:53:11 -0300

    Add files via upload

---
+++ pythonProject/main.py
@@ -0,0 +1,54 @@
+def txt_to_bin(texto):
+    binario = ''
+    for caractere in texto:
+        valor_ascii = ord(caractere)
+        binario += format(valor_ascii, '08b') + ' '
+    return binario
+
+
+def bin_to_txt(binario):
+    caracteres = binario.split()
+    texto = ''
+    for caractere_binario in caracteres:
+        valor_decimal = int(caractere_binario, 2)
+        caractere = chr(valor_decimal)
+        texto += caractere
+    return texto
+
+
+def num_to_bin(numero):
+    numero = int(numero)
+    binary = bin(abs(numero))
+    return binary[2:]
+
+
+def retornar():
+    print("Do you want to confirm what do you just write")
+    confirm = int(input("0 = Y / 1 = N"))
+    if confirm == 0:
+        in_put = input("Write what you got")
+        print("The your printed text: " , bin_to_txt(in_put))
+
+
+# print a chose
+print("What do you want to do")
+a = int(input("1 to write ===== 2 to read ===== or 3 just convert numbers to bin"))
+
+if a == 1:
+    texto = input("Write what do you want to convert to Binary")
+    resultado = txt_to_bin(texto)
+    print("Binary says: ", resultado)
+    retornar()
+
+elif a == 2:
+    binario = input("Write what do you want to convert to Text")
+    resultado = bin_to_txt(binario)
+    print("Texto: " , resultado)
+
+elif a == 3:
+    numero = input("Write THE NUMBER you want convert")
+    resultado = num_to_bin(numero)
+    print(numero,"in Binary is: ", resultado)
+else:
+    print("something gets wrong")
+
